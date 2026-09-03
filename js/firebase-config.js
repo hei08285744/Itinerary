23fd3bd1916e1becb130855efcc56254ba43f9ac
hei08285744 <33191146+hei08285744@users.noreply.github.com>
2026-09-04 03:18:27 +0800

    Fix Google OAuth redirect domain

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,6 @@
 window.FIREBASE_CONFIG = {
   apiKey: 'AIzaSyAalaq5_dUbsIpy0hkh2Jfo3keCyIfgm7k',
-  authDomain: 'itinerary-hei08285744.web.app',
+  authDomain: 'itinerary-hei08285744.firebaseapp.com',
   projectId: 'itinerary-hei08285744',
   storageBucket: 'itinerary-hei08285744.firebasestorage.app',
   messagingSenderId: '99018691027',
